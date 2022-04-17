4975eee02ae19c92f1376f741bcc5cfaf4482c81
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-04-17 12:25:08 +0600

    login and register design done

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import "./Login.css";
+import google from "../../image/google.png"
+import twitter from "../../image/twitter.png"
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div style={{ marginTop: "200px" }} className='d-flex justify-content-center'>
-            <form>
-                <input type="email" name="email" id="" required /> <br />
-                <input type="password" name="password" id="" required /><br />
-                <input type="password" name="confirmPassword" id="" required /><br />
-                <input type="submit" value="Login" /> <br />
-            </form>
+        <div>
+            <div style={{ marginTop: "50px" }} className='d-flex justify-content-center'>
+                <form>
+                    <h1 className='text-center text-primary'>Please Login</h1> <br />
+                    <input className='my-2 py-2 login-field' type="email" name="email" placeholder='Enter Your Email' required /> <br />
+                    <input className='my-2 py-2 login-field' type="password" name="password" placeholder='Enter Your Password' required /><br />
+                    <input className='my-2 py-2 bg-primary border-0 mx-auto w-25 d-block rounded-pill fw-bold text-white' type="submit" value="Login" /> <br />
+                    <Link to="/register" className='text-primary text-center d-block text-decoration-none fw-bold'>New Here? Create An Account</Link>
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <hr className='w-25 mx-3' />
+                        <p className='text-muted pb-1 my-0'>or</p>
+                        <hr className='w-25 mx-3' />
+                    </div>
+                </form>
+            </div>
+            <button className='mx-auto d-block my-3 bg-white text-primary fw-bold py-3 px-5 border-primary rounded-pill border-1'> <img className='logo' src={google} alt="" /> Continue With Google</button>
+            <button className='mx-auto d-block my-3 bg-white text-primary fw-bold py-3 px-5 border-primary rounded-pill border-1'> <img className='logo' src={twitter} alt="" /> Continue With Twitter</button>
         </div>
     );
 };
---
+++ src/Component/Register/Register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+    return (
+        <div>
+            <div style={{ marginTop: "50px" }} className='d-flex justify-content-center'>
+                <form>
+                    <h1 className='text-center text-primary'>Please Register</h1> <br />
+                    <input className='my-2 py-2 login-field' type="email" name="email" id="" placeholder='Enter Your Email' required /> <br />
+                    <input className='my-2 py-2 login-field' type="password" name="password" id="" placeholder='Enter Your Password' required /><br />
+                    <input className='my-2 py-2 login-field' type="password" name="confirmPassword" id="" placeholder='Enter The Same Password' required /><br />
+                    <input className='my-2 py-2 bg-primary border-0 mx-auto w-25 d-block rounded-pill fw-bold text-white' type="submit" value="Sign Up" /> <br />
+                    <Link to="/login" className='text-primary text-center d-block text-decoration-none fw-bold'>Already Have An Account ?</Link>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
