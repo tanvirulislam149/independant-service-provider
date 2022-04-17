cb7897bad33df00043d9ea7eec9fd8217243c9d7
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-04-18 01:44:40 +0600

    about page done

--- src/Component/AboutMe/AboutMe.js
+++ src/Component/AboutMe/AboutMe.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const AboutMe = () => {
     return (
-        <div className='mt-5'>
-            <h2>About Me</h2>
+        <div className='container w-75 text-center mt-5'>
+            <img className='w-25 rounded-pill' src="https://www.millsriverdental.com/wp-content/uploads/2021/03/Dr.-Michael-Stohl-Dentist-Mills-River-NC-scaled-e1616518282883.jpg" alt="" />
+            <h2 className='text-primary mt-3'>Dr. Mills River</h2>
+            <h4>I want to make sure that no one suffers from tooth problem. So, I have a plan to make a high quality dental hospital so that everyone can have healthy tooth. </h4>
         </div>
     );
 };
